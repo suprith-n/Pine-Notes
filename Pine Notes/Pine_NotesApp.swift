b432ad5ee7797358eaa315d4c39ddb393f689d28
suprith-n <ravi.suprith17@gmail.com>
2024-10-09 00:50:12 +0530

    Initial Commit

---
+++ Pine Notes/Pine_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pine_NotesApp.swift
+//  Pine Notes
+//
+//  Created by Heavy on 09/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Pine_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
