e109bc1b4666e2409fe8ba2e9a1a67c387d7fbd3
suprith-n <ravi.suprith17@gmail.com>
2024-10-09 02:29:09 +0530

    Welcome Message with gradient added

--- Pine Notes/ContentView.swift
+++ Pine Notes/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        GeometryReader { geometry in
+            RoundedRectangle(cornerRadius: 25.0)
+                .fill(
+                    LinearGradient(
+                        gradient: Gradient(colors: [Color.green.opacity(0.9), Color.black.opacity(0.6), Color.green.opacity(0.8)]),
+                        startPoint: .topLeading, endPoint: .bottomTrailing)
+                )
+                .frame(height: geometry.size.height/2)
+                .overlay(RoundedRectangle(cornerRadius: 25.0)
+                    .fill(Color.black.opacity(0.6))
+                    .background(
+                                RoundedRectangle(cornerRadius: 25)
+                                            .fill(Color.black.opacity(0.6))
+                                            .blur(radius: 90, opaque: false) // Apply blur effect
+                                    )
+                    .frame(width: .infinity, height: 200)
+                    .overlay(
+                        HStack {
+                            VStack(alignment: .leading,
+                                   spacing: 15) {
+                            Text("Welome to Pine Notes Heavy")
+                                .font(.system(size: 24, weight: .bold))
+                            Text("Your Personal space for Ideating and noting thoughts")
+                                .font(.system(.subheadline))
+                            }
+                            Image(systemName: "applepencil.and.scribble")
+                                .imageScale(.large)
+                                .font(.system(size: 40))
+                        }.padding()
+                    )
+                    .padding()
+                    .foregroundColor(.white)
+                )
         }
-        .padding()
+        .edgesIgnoringSafeArea(.top)
     }
 }
 
