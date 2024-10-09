b7885a39f94df3c2d53063a602d89722acfbf7f9
suprith-n <ravi.suprith17@gmail.com>
2024-10-10 02:09:21 +0530

    note row added

---
+++ Pine Notes/Note.swift
@@ -0,0 +1,8 @@
+//
+//  Note.swift
+//  Pine Notes
+//
+//  Created by Heavy on 10/10/24.
+//
+
+import Foundation
---
+++ Pine Notes/NoteRow.swift
@@ -0,0 +1,18 @@
+//
+//  NoteRow.swift
+//  Pine Notes
+//
+//  Created by Heavy on 10/10/24.
+//
+
+import SwiftUI
+
+struct NoteRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteRow()
+}
